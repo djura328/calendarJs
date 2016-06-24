85b49fc9242c6d7c8ce591810b864e5c1d3162fb
djura328 <djura328@yahoo.com>
2016-06-24 16:33:37 +0200

    napravlejn update

--- save.php
+++ save.php
@@ -1,7 +1,11 @@
 <?php
-//echo $_POST['name_tv'];
-//echo $_POST['name_emission'];
-//echo $_POST['brClanaka'];
-echo " " . $_POST['brClanaka'];
-echo " " . $_POST['trajanje'];
+
+include "connector.php";
+
+$brClanaka=$_POST['brClanaka'];
+$trajanje=$_POST['trajanje'];
+$idUser=$_POST['idUser'];
+$idArticle=$_POST['idArticle'];
+$date=$_POST['date'];
+mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka' WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
 ?>
