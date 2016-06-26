e0a2e88a7bc1252047a743eb43f6be841a1305a1
djura328 <djura328@yahoo.com>
2016-06-27 00:01:34 +0200

    radi update sa ajaxom

--- save.php
+++ save.php
@@ -7,5 +7,5 @@ $trajanje=$_POST['trajanje'];
 $idUser=$_POST['idUser'];
 $idArticle=$_POST['idArticle'];
 $date=$_POST['date'];
-mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka' WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
+mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka', status='edited' WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
 ?>
