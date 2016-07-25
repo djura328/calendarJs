acb55b4c3a0c0d60400e7ca8831455b0318713e7
djura328 <djura328@yahoo.com>
2016-07-25 20:57:26 +0200

    nista pormenjeno

--- add_emission.php
+++ add_emission.php
@@ -7,17 +7,17 @@ include "connector.php";
 			$name_emission=$_POST['name_emission'];
 			$brClanaka=$_POST['brClanaka'];
 			$duration=$_POST['duration'];
-			$da=$_POST['datum'];
+			echo $da=$_POST['datum'];
 				$dan=date('l',strtotime($da));
-			$idUser=$_POST['idUser'];
-			$idEmission=$_POST['idEmission'];
+			echo $idUser=$_POST['idUser'];
+			echo $idEmission=$_POST['idEmission'];
 			$selekt=$_POST['se'];
 			$napomena=$_POST['napomena'];
 			
 			//$ch=mysqli_query($link, "SELECT id FROM `broadcast` WHERE date='$da' AND id_user='$idUser' AND id_emission='$idEmission'");
 			$ch=mysqli_query($link, "SELECT id FROM `broadcast` WHERE date='$da' AND id_emission='$idEmission'");
 			if(mysqli_num_rows($ch)==1){
-				$info="emisija je vec uradjena";
+				//echo $info="emisija je vec uradjena";
 				header("Location: index2.php?datum=$da&info=$info");
 				//echo "1";
 			}
@@ -25,12 +25,12 @@ include "connector.php";
 				if($selekt==0){
 				mysqli_query($link, "INSERT INTO `broadcast` (id_user, id_emission, date, duration, article, status, napomena) VALUES ('$idUser', '$idEmission', '$da', '$duration', '$brClanaka', 'complete', '$napomena')");
 				header("Location: index2.php?datum=$da");
-				//echo "2";
+				echo "2";
 				}
 				else{
 				mysqli_query($link, "INSERT INTO `broadcast` (id_user, id_emission, date, duration, article, status, napomena) VALUES ('$idUser', '$idEmission', '$da', '0', '0', 'stuck', '$napomena')");
 				header("Location: index2.php?datum=$da");
-				//cho "3";
+				//echo "3";
 				}
 			}
 			
