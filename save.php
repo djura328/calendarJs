34b625e1b9ed229921475f54c2aafa7f9fab0e28
djura328 <djura328@yahoo.com>
2016-08-04 16:09:23 +0200

    dodat snimanje date publish

--- save.php
+++ save.php
@@ -7,6 +7,7 @@ $trajanje=$_POST['trajanje'];
 $idUser=$_POST['idUser'];
 $idArticle=$_POST['idArticle'];
 $date=$_POST['date'];
+$date_publish=$_POST['date_publish'];
 $ch1=mysqli_query($link, "SELECT id FROM `user` WHERE id=$idUser");
 $res1=mysqli_fetch_array($ch1);
 $ch2=mysqli_query($link, "SELECT id_user FROM `broadcast` WHERE id_emission=$idArticle AND date='$date'");
@@ -15,11 +16,11 @@ $ch3=mysqli_query($link, "SELECT duration FROM `emission` WHERE id_user=$idUser 
 $res3=mysqli_fetch_array($ch3);
 if($res1['id']==$res2['id_user']){
 	if($trajanje!=$res3['duration']){
-	mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka', status='changed', napomena=''	WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
+	mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka', status='changed', napomena='', date_publish='$date_publish'	WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
 	echo "Uspesno prmenjeno";
 	}
 	else{
-	mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka', status='edited', napomena='' WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
+	mysqli_query($link, "UPDATE `broadcast` SET duration='$trajanje', article='$brClanaka', status='edited', napomena='', date_publish='$date_publish' WHERE id_user='$idUser' AND id_emission='$idArticle' AND date='$date'");
 	echo "Uspesno prmenjeno";
 	}
 }
