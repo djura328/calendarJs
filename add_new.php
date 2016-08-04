abdc61d419621d0e01160c25a8e02e7f46a39b3e
djura328 <djura328@yahoo.com>
2016-08-04 16:31:02 +0200

    dodata nasa slova

--- add_new.php
+++ add_new.php
@@ -1,5 +1,6 @@
 <?php
 include "connector.php";
+mysqli_set_charset($link,"utf8");
 $name=$_POST['name'];
 $query=mysqli_query($link, "SELECT name_emission FROM `emission` WHERE name_tv='$name'");
 echo "<option></option>";
